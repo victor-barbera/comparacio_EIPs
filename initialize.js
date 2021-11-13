f9bf1dbd6ee5725d487f4daa794f2e0599a513b7
Víctor Barberà <vcitor.bq@gmail.com>
2021-11-13 17:20:56 +0100

    primer commit

---
+++ initialize.js
@@ -0,0 +1,9 @@
+
+    window.onload = function() {
+        if (typeof web3 !== 'undefined') {
+          web3 = new Web3(web3.currentProvider);
+        } else {
+          web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"));
+        }
+        console.log(web3.version.api);
+      }
