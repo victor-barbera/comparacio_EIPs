1a9c9eaa59d972ed7e773bd0fd07c5e3786a6817
adriagc8 <adrigonzalezcopado@gmail.com>
2021-11-23 10:38:34 +0100

    trasnfer signed example

--- node-app/app.js
+++ node-app/app.js
@@ -3,7 +3,8 @@ if (typeof window.ethereum !== 'undefined') console.log('MetaMask is installed :
 else console.warn('MetaMask is not installed :(');
 
 
-document.getElementById("metamask-connect").addEventListener('click', ()=> {
+document.getElementById("metamask-connect").addEventListener('click', async ()=> {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     console.table(accounts);
+    
 });
