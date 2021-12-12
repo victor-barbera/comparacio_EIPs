4571cc70a6e659e39dd5862290c0172fc80a837a
adriagc8 <adrigonzalezcopado@gmail.com>
2021-12-12 20:58:03 +0100

    transactions working

--- nomes-client/transferExample.js
+++ nomes-client/transferExample.js
@@ -1,43 +1,147 @@
-// ABI to transfer ERC20 Token
-let abi = JSON.parse(fs.readFileSync(path.join(__dirname, '.', 'main_contract.json'), 'utf-8'));
-// calculate ERC20 token amount ( 18 decimals )
-let amount = 1;
-let tokenAmount = web3.utils.toWei(amount.toString(), 'ether')
-// Get ERC20 Token contract instance
-let contract = new web3.eth.Contract(abi, tokenAddress, {
+
+    const fromAddress='0xDa5E6B7f172847d63676aA6aC51e108a06c2044D';
+    const SMART_CONTRACT_ADDRESS = "0xbf7c4DEF48D4314dF20BC77A98b86400B3d8C73C";
+    //const { FT_ABI } = import('./FT_ABI.json');
+    const FT_ABI= [
+
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "_from",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "_to",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Transfer",
+            "type": "event"
+        },
+        { "constant": true,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+          "constant": true,
+            "inputs": [],
+            "name": "totalSupply",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "_to",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transfer",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "success",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "payable",
+            "type": "function"
+        }
+    ]
+        
+web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/15a6a4951e1345ebaecccbf617140e64"));
+
+contract = new web3.eth.Contract(FT_ABI, SMART_CONTRACT_ADDRESS, {
     from: fromAddress
 });
+
+//contract = web3.eth.contract(FT_ABI).at(SMART_CONTRACT_ADDRESS);
+console.log(contract);
+let totalSupply = await contract.methods.totalSupply().call();
+console.log("TOTAL SUPPLY",totalSupply)
+
+//let tokenAddress = "0xbf7c4DEF48D4314dF20BC77A98b86400B3d8C73C";
+let toAddress = "0xDeCD84708c75258F8D491A324077fFd846C1884c";
+// calculate ERC20 token amount ( 18 decimals )
+let amount = 1;
+console.log("amount: " + amount);
+let tokenAmount = web3.utils.toWei(amount.toString(), 'ether')
+console.log("token amount: " + tokenAmount);
+
 // How many tokens do I have before sending?
 let balance = await contract.methods.balanceOf(fromAddress).call();
 console.log(`Balance before send: ${balance}`);
 // EIP 155 - List of Chain ID's:
-const chainList = {
-    mainnet: 1,
-    morden: 2,
-    ropsten: 3,
-    rinkeby: 4,
-    ubiqMainnet: 8,
-    ubiqTestnet: 9,
-    rootstockMainnet: 30,
-    rootstockTestnet: 31,
-    kovan: 42,
-    ethereumClassicMainnet: 61,
-    ethereumClassicTestnet: 62,
-    ewasmTestnet: 66,
-    gethPrivateChains: 1337
-};
+// const chainList = {
+//     mainnet: 1,
+//     morden: 2,
+//     ropsten: 3,
+//     rinkeby: 4,
+//     ubiqMainnet: 8,
+//     ubiqTestnet: 9,
+//     rootstockMainnet: 30,
+//     rootstockTestnet: 31,
+//     kovan: 42,
+//     ethereumClassicMainnet: 61,
+//     ethereumClassicTestnet: 62,
+//     ewasmTestnet: 66,
+//     gethPrivateChains: 1337
+// };
 // The gas price is determined by the last few blocks median gas price.
 const avgGasPrice = await web3.eth.getGasPrice();
 // current transaction gas prices from https://ethgasstation.info/
-const currentGasPrices = await getCurrentGasPrices();
+//const currentGasPrices = await getCurrentGasPrices();
+
 /**
  * With every new transaction you send using a specific wallet address,
  * you need to increase a nonce which is tied to the sender wallet.
  */
-let nonce = web3.eth.getTransactionCount(fromAddress);
+
+let nonce = await web3.eth.getTransactionCount(fromAddress);
+
+
 // Will call estimate the gas a method execution will take when executed in the EVM without.
 let estimateGas = await web3.eth.estimateGas({
-    "value": '0x0', 
+    "value": web3.utils.toHex(amount), 
     "data": contract.methods.transfer(toAddress, tokenAmount).encodeABI(),
     "from": fromAddress,
     "to": toAddress
@@ -57,18 +161,21 @@ const transaction = {
     "gasPrice": web3.utils.toHex(Math.trunc(currentGasPrices.medium * 1e9)),
     "chainId": web3.utils.toHex(chainList.mainnet)
 };*/
-let lastCountOfTransaction = await web3.eth.getTransactionCount(fromAddress)
 
 const transaction = {
-    "value": '0x01',                            
-    "nonce": '0x' + lastCountOfTransaction.toString(16),      
-    "contractAddress":"0xd9145CCE52D386f254917e481eB44e9943F39138",
-    "data": contract.methods.transfer(fromAddress, toAddress, tokenAmount).encodeABI(),
+    "value": web3.utils.toHex(amount),                           
+    "nonce": web3.utils.toHex(nonce),      
+    //"contractAddress":"0xd9145CCE52D386f254917e481eB44e9943F39138",
+    "data": contract.methods.transfer( toAddress, tokenAmount).encodeABI(),
     "from": fromAddress,
     "to": toAddress,
     "gas": web3.utils.toHex(24200)
 };
 // Creates an account object from a private key.
+
+// Addressa amb la que sa connectat el remix al ( metamask )
+
+let privateKey = "3b7159170c5041d497bcb1788f27ed2e8efe89cf4be17afb980e709c023f3033";
 const senderAccount = web3.eth.accounts.privateKeyToAccount(privateKey);
 /**
 * This is where the transaction is authorized on your behalf.
